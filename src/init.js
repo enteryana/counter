21c430f15bd209a901dfb73c38443e295ef74531
enteryana <valchenkoyana@gmail.com>
2024-03-28 14:53:26 +0100

    add counter

--- src/init.js
+++ src/init.js
@@ -1,1 +1,29 @@
 alert('init.js loaded!');
+
+const data = {
+    count: 0
+}
+
+const display = document.getElementById("display");
+const increment = document.getElementById("pls");
+const decrement = document.getElementById("min");
+const reset = document.getElementById("reset");
+
+const updateDisplay = () => {
+    display.innerText = data.count;
+}
+
+pls.addEventListener('click', () =>  {
+    data.count += 1;
+    updateDisplay();
+})
+
+min.addEventListener('click', () => {
+    data.count -= 1;
+    updateDisplay();
+})
+
+reset.addEventListener('click', () => {
+    data.count = 0;
+    updateDisplay();
+})
